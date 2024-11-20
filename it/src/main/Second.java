1f0dda41df0d67306434ef092675376cb9ea0b87
sathwikagannamaneni <gannamanenisathwika12@gmail.com>
2024-11-20 11:39:33 +0530

    Add second file.java and third file.java

---
+++ it/src/main/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+    public Static void main(String[] args)
+    {
+        System.out.println("Second.java");
+    }
+    
+}
---
+++ it/src/main/Third.java
@@ -0,0 +1,7 @@
+public class Third {
+    public Static void main(String[] args)
+    {
+        System.out.println("Third.java");
+    }
+    
+}
